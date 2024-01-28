15c81d47739bfbb7593d7b1c454fa1a66dbb6727
Gabriel Ong <gabrielongxe@gmail.com>
2024-01-28 21:17:32 +0800

    update font family for all styled components

--- src/components/styled/AnswerDiv.jsx
+++ src/components/styled/AnswerDiv.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const AnswerDiv = styled.div`
+  font-family: 'Press Start 2P', cursive;
   display: flex;
   flex-direction: row;
   align-items: center;
